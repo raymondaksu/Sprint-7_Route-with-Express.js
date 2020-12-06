fe15e6fff76bd757c94093b11ad16557aaf12834
raymondaksu <raymondaksu@users.noreply.github.com>
2020-12-06 15:38:22 +0300

    Routes is imple

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+
+const port = process.env.PORT || 5001;
+const host = "localhost";
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname + "/views"));
+
+//Public folder is being convert static
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/", (req, res) => {
+    res.render("home");
+})
+
+app.get("/register", (req, res) => {
+    res.render("register");
+})
+
+app.listen(port, (req, res) => {
+    console.log(`I'm listening http://${host}:${port}`)
+})
